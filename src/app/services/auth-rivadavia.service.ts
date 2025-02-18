9d353ae42d8d72fc3bfc392996b86fee46b94163
Facc <facc.15@live.com.ar>
2025-02-18 11:07:37 -0300

    add netlify functions

--- src/app/services/auth-rivadavia.service.ts
+++ src/app/services/auth-rivadavia.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,23 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthRivadaviaService {
-  private apiUrl = 'https://ssotest.apps.segurosrivadavia.com/auth/realms/api-brokers/protocol/openid-connect/token';
+  private netlifyFunctionUrl = 'https://tecnicayservicios.netlify.app/.netlify/functions/getTokenRivadavia';
 
   constructor(private http: HttpClient) {}
 
   getToken(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Ocp-Apim-Subscription-Key': 'c6b637fc51',
-    });
-
-    const body = new HttpParams()
-      .set('client_id', '24bea14a')
-      .set('client_secret', 'e946749e9e1cdce8a8709b5604a3e0e5')
-      .set('grant_type', 'password')
-      .set('username', 'tsgr')
-      .set('password', 'vbVdGFsWnQ81Dg9');
-
-    return this.http.post(this.apiUrl, body, { headers });
+    return this.http.get(this.netlifyFunctionUrl);
   }
 }
