aef43a1ceef04d1a01f9513bf0cbb04c078f9b34
facc15 <facc.15@live.com.ar>
2025-02-28 18:48:16 -0300

    add layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,6 @@ export class AppComponent implements OnInit {
   isNavigating = false;
 
   constructor(
-
     private router: Router,
     private authService: AuthService,
     private cdr: ChangeDetectorRef
