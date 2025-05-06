a1f6bbc1050ffaafb164f5ca4c4b0acc2902a110
Facundo <facc.15@live.com.ar>
2025-05-06 17:17:47 -0300

    fix atm service5

--- backend-atm/atm-service.ts
+++ backend-atm/atm-service.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 import { Builder } from "xml2js";
 const ATM_SOAP_URL = "https://wsatm-dev.atmseguros.com.ar/index.php/soap";
 
-export const cotizarATMXML= async (xmlObject: any) => {
+export const cotizarATMXML= async (xml: string) => {
   try {
     console.log("🕓 [ATM] Fecha y hora:", new Date().toISOString());
     // 🔁 Convertir objeto JS a string XML
     const builder = new Builder({ headless: true }); // headless = sin <?xml ...?>
-    const xmlString = builder.buildObject(xmlObject);
-    const response = await axios.post(ATM_SOAP_URL, xmlString, {
+
+    const response = await axios.post(ATM_SOAP_URL, xml, {
       headers: {
         "Content-Type": "text/xml; charset=utf-8",
         "SOAPAction": "http://tempuri.org/AUTOS_Cotizar", // importante
--- backend-atm/dist/index.js
+++ backend-atm/dist/index.js
@@ -6,15 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const atm_service_1 = require("./atm-service");
-const express_xml_bodyparser_1 = __importDefault(require("express-xml-bodyparser"));
 const app = (0, express_1.default)();
 const PORT = Number(process.env["PORT"]) || 3000;
 // ✅ Middleware
 app.use((0, cors_1.default)({ origin: "*", methods: ["POST"] }));
 app.use(express_1.default.text({ type: "text/xml" }));
-app.use(express_1.default.json());
-// Configurar body-parser para manejar XML
-app.use((0, express_xml_bodyparser_1.default)());
 // ✅ Endpoint principal
 app.post("/ATM/cotizar", async (req, res) => {
     try {
