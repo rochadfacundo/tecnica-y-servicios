c1e0f1e144e3a47d37af948608f18e2bb233535e
Facundo <facc.15@live.com.ar>
2025-05-06 17:04:41 -0300

    fix atm service

--- backend-atm/dist/index.js
+++ backend-atm/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const atm_service_1 = require("./atm-service");
 const express_xml_bodyparser_1 = __importDefault(require("express-xml-bodyparser"));
 const app = (0, express_1.default)();
 const PORT = Number(process.env["PORT"]) || 3000;
@@ -18,8 +19,8 @@ app.use((0, express_xml_bodyparser_1.default)());
 app.post("/ATM/cotizar", async (req, res) => {
     try {
         const xmlData = req.body;
-        // Procesar xmlData según sea necesario
-        res.status(200).send(xmlData);
+        const response = (0, atm_service_1.cotizarATMXML)(xmlData);
+        res.status(200).send(response);
     }
     catch (error) {
         console.error("Error al procesar la solicitud:", error);
