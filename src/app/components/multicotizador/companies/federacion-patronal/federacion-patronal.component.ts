9883e9ea7743a2b445b8ae2c365377aea855ed64
Facundo <facc.15@live.com.ar>
2025-05-20 15:00:27 -0300

    add companies components

---
+++ src/app/components/multicotizador/companies/federacion-patronal/federacion-patronal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-federacion-patronal',
+  standalone: true,
+  imports: [],
+  templateUrl: './federacion-patronal.component.html',
+  styleUrl: './federacion-patronal.component.css'
+})
+export class FederacionPatronalComponent {
+
+}
---
+++ src/app/components/multicotizador/companies/rivadavia/rivadavia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rivadavia',
+  standalone: true,
+  imports: [],
+  templateUrl: './rivadavia.component.html',
+  styleUrl: './rivadavia.component.css'
+})
+export class RivadaviaComponent {
+
+}
