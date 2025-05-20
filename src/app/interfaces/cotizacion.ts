6ea8396437fc4f5a86b05e8a94a1238afee0d5d8
Facundo <facc.15@live.com.ar>
2025-05-20 17:19:32 -0300

    add quote table

---
+++ src/app/interfaces/cotizacion.ts
@@ -0,0 +1,8 @@
+export interface Cotizacion {
+  compania: string;
+  rc: number;
+  mb: number;
+  mplus: number;
+  tr1: number;
+  tr2: number;
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,21 +3,25 @@ import { Component } from '@angular/core';
 import { MulticotizadorComponent } from '../../components/multicotizador/multicotizador.component';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { TablaCotizadoraComponent } from '../../components/multicotizador/tabla-cotizadora/tabla-cotizadora.component';
+import { Cotizacion } from '../../interfaces/cotizacion';
+import { getCotizacionesTabla } from '../../components/multicotizador/utils/utils';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule,MulticotizadorComponent],
+  imports: [CommonModule,MulticotizadorComponent,TablaCotizadoraComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
   multicotizador:boolean=false;
+  misCotizaciones:Cotizacion[];
 
   constructor(private s_auth:AuthService,private router:Router)
   {
-
+    this.misCotizaciones= getCotizacionesTabla();
   }
 
   logout()
