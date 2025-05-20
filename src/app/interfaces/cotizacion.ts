4e4dcc1a621c7e76466d7dc35212bc9f10dd2438
Facundo <facc.15@live.com.ar>
2025-05-20 17:44:36 -0300

    add tablaCotizadoraComponent

--- src/app/interfaces/cotizacion.ts
+++ src/app/interfaces/cotizacion.ts
@@ -1,8 +1,8 @@
 export interface Cotizacion {
   compania: string;
-  rc: number;
-  mb: number;
-  mplus: number;
-  tr1: number;
-  tr2: number;
+  rc?: number;
+  mb?: number;
+  mplus?: number;
+  tr1?: number;
+  tr2?: number;
 }
